d35ba4eb59d0b2898754e178603197b430303679
empdo <emil.essung@live.se>
2022-03-08 10:44:37 +0100

    add saveas

--- src/index.ts
+++ src/index.ts
@@ -244,7 +244,7 @@ pool.getConnection()
             const conversation = await getConversation(conn, userId, id);
             const user = await getUser(conn, id);
 
-            if(user["name"]) {
+            if(user) {
                 res.send({ messages: conversation, name: user["name"], picture: user["picture"]});
             }else {
                 res.sendStatus(202)
