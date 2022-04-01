340bd801fce8a3bdc8ff909d16131446060fba7d
empdo <emil.essung@live.se>
2022-04-01 12:11:41 +0200

    add ws for profilepic change

--- src/index.ts
+++ src/index.ts
@@ -355,7 +355,7 @@ try {
         await conn.query(`UPDATE users SET picture="${id}.png" WHERE id=${id}`);
       }
 
-      em.emit("picChange", { id });
+      em.emit("picChange", id);
       res.sendStatus(200);
     });
 
